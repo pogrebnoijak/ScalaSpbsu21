aed3541b6175ed4a5a1f23a4d6c5658975051f1e
andrew <p-a-k-12@mail.ru>
2021-02-20 16:24:14 +0300

    correction of blots

--- 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
+++ 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
@@ -7,16 +7,16 @@ sealed trait IntList {
   def drop(n: Int): IntList
   def take(n: Int): IntList
   def map(f: Int => Int): IntList
-  def ::(elem: Int): IntList                  = List(elem, IntNil)
+  def ::(elem: Int): IntList                  = HList(elem, IntNil)
   def foldLeft[B](z: B)(op: (B, Int) => B): B = this match {
     case IntNil => z
-    case List(x, xs) => xs.foldLeft(op(z, x)){op}
+    case HList(x, xs) => xs.foldLeft(op(z, x))(op)
   }
 }
 
 object IntList {
   def undef: Nothing = throw new UnsupportedOperationException("operation is undefined")
-  def fromSeq(seq: Seq[Int]): IntList = seq.foldRight(IntNil:IntList){List}
+  def fromSeq(seq: Seq[Int]): IntList = seq.foldRight(IntNil:IntList){HList}
   def sum(intList: IntList): Int      = intList match {
     case IntNil => undef
     case _ => intList.foldLeft(0){_ + _}
@@ -24,15 +24,15 @@ object IntList {
   def size(intList: IntList): Int     = intList.foldLeft(0){(a, _) => a + 1}
 }
 
-case class List(x: Int, xs: IntList) extends IntList {
-  override def head: Int                    = this match { case List(x, _) => x }
-  override def tail: IntList                = this match { case List(_, xs) => xs }
+case class HList(x: Int, xs: IntList) extends IntList {
+  override def head: Int                    = x
+  override def tail: IntList                = xs
   override def drop(n: Int): IntList        = if (n <= 0) this else tail.drop(n-1)
   override def take(n: Int): IntList        = this match {
     case _ if n <= 0 => IntNil
-    case List(x,xs) => List(x, xs.take(n-1))
+    case HList(x,xs) => HList(x, xs.take(n-1))
   }
-  override def map(f: Int => Int): IntList  = this match { case List(x, xs) => List(f(x), xs.map(f)) }
+  override def map(f: Int => Int): IntList  = this match { case HList(x, xs) => HList(f(x), xs.map(f)) }
 }
 
 case object IntNil extends IntList {
