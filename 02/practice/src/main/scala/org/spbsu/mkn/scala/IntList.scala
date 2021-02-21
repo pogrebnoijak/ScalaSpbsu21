f8e8dcd8d912ccb0ec9b607e99d53fee96c1d355
andrew <p-a-k-12@mail.ru>
2021-02-21 21:42:58 +0300

    correction, delete extra matches

--- 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
+++ 02/practice/src/main/scala/org/spbsu/mkn/scala/IntList.scala
@@ -9,17 +9,17 @@ sealed trait IntList {
   def map(f: Int => Int): IntList
   def ::(elem: Int): IntList                  = HList(elem, IntNil)
   def foldLeft[B](z: B)(op: (B, Int) => B): B = this match {
-    case IntNil => z
+    case IntNil       => z
     case HList(x, xs) => xs.foldLeft(op(z, x))(op)
   }
 }
 
 object IntList {
   def undef: Nothing = throw new UnsupportedOperationException("operation is undefined")
-  def fromSeq(seq: Seq[Int]): IntList = seq.foldRight(IntNil:IntList){HList}
+  def fromSeq(seq: Seq[Int]): IntList = seq.foldRight(IntNil: IntList)(HList)
   def sum(intList: IntList): Int      = intList match {
     case IntNil => undef
-    case _ => intList.foldLeft(0){_ + _}
+    case _      => intList.foldLeft(0){_ + _}
   }
   def size(intList: IntList): Int     = intList.foldLeft(0){(a, _) => a + 1}
 }
@@ -28,11 +28,8 @@ case class HList(x: Int, xs: IntList) extends IntList {
   override def head: Int                    = x
   override def tail: IntList                = xs
   override def drop(n: Int): IntList        = if (n <= 0) this else tail.drop(n-1)
-  override def take(n: Int): IntList        = this match {
-    case _ if n <= 0 => IntNil
-    case HList(x,xs) => HList(x, xs.take(n-1))
-  }
-  override def map(f: Int => Int): IntList  = this match { case HList(x, xs) => HList(f(x), xs.map(f)) }
+  override def take(n: Int): IntList        = if (n <= 0) IntNil else HList(x, xs.take(n-1))
+  override def map(f: Int => Int): IntList  = HList(f(x), xs.map(f))
 }
 
 case object IntNil extends IntList {
