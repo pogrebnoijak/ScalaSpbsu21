bae440553964eb4bc7e840ecb168c5d8434e96b4
andrew <p-a-k-12@mail.ru>
2021-03-04 20:11:32 +0300

    + filter, plus, sort, sum addition, comparators, tests for everything

---
+++ 04/practice/src/main/scala/org/spbsu/mkn/scala/MyGenericList.scala
@@ -0,0 +1,67 @@
+package org.spbsu.mkn.scala
+
+import org.spbsu.mkn.scala.MyGenericList.{MyNil, sum, turnerInt, undef}
+
+import java.util.Comparator
+
+sealed trait MyGenericList[+T] {
+  def head: T
+  def tail: MyGenericList[T]
+  def drop(n: Int): MyGenericList[T]
+  def take(n: Int): MyGenericList[T]
+  def map[T2](f: T => T2): MyGenericList[T2]
+  def filter[T2](f: T => Boolean): MyGenericList[T2]
+  def ::[T2 >: T](elem: T2): MyGenericList[T2]  = HList[T2](elem, MyNil)
+  def foldLeft[B](z: B)(op: (B, T) => B): B     = this match {
+    case HList(x, xs) => xs.foldLeft(op(z, x))(op)
+    case MyNilList    => z
+  }
+}
+
+object MyGenericList {
+  def undef: Nothing                                = throw new UnsupportedOperationException("operation is undefined")
+  def fromSeq[T](seq: Seq[T]): MyGenericList[T]     = seq.foldRight(MyNil[T])(HList[T])
+  def size[T](genList: MyGenericList[T]): Int       = genList.foldLeft(0){(a, _) => a + 1}
+  def sum[T, B](genList: MyGenericList[T])(b: B = 0)(implicit turn: (B,T) => B): B = genList match {
+    case HList(_, _)  => genList.foldLeft(b)((bb, t) => turn(bb, t))
+    case MyNilList    => undef
+  }
+  def MyNil[T]: MyGenericList[T]                    = MyNilList.asInstanceOf[MyGenericList[T]]
+  def sort[T](list: MyGenericList[T])(implicit comparator: Comparator[T]): MyGenericList[T] = list match {
+    case HList(head, tail) => plus(sort(tail.filter(comparator.compare(head, _) <= 0))(comparator),
+      HList(head, sort(tail.filter(comparator.compare(head, _) > 0))(comparator)))
+    case MyNilList         => MyNilList
+  }
+  def plus[T](list1: MyGenericList[T], list2: MyGenericList[T]): MyGenericList[T] = list1 match {
+    case HList(head, tail)  => HList(head, plus(tail, list2))
+    case MyNilList          => list2
+  }
+
+  implicit def ComparatorGList[T]: Comparator[MyGenericList[T]] =
+    (a: MyGenericList[T], b: MyGenericList[T])  => size(a) compare size(b)
+  implicit def turnerInt: (Int, Int)            => Int = {_ + _}
+  implicit def turnerStr: (Int, String)         => Int = {_ + _.length}
+}
+
+case class HList[T](head: T, tail: MyGenericList[T]) extends MyGenericList[T] {
+  override def drop(n: Int): MyGenericList[T]                 = if (n <= 0) this else tail.drop(n-1)
+  override def take(n: Int): MyGenericList[T]                 = if (n <= 0) MyNil else HList(head, tail.take(n-1))
+  override def map[T2](f: T => T2): MyGenericList[T2]         = HList(f(head), tail.map(f))
+  override def filter[T2](f: T => Boolean): MyGenericList[T2] = if (f(head)) HList(head.asInstanceOf[T2], tail.filter(f))
+                                                                else tail.filter(f)
+}
+
+case object MyNilList extends MyGenericList[Nothing] {
+  override def head: Nothing                                      = undef
+  override def tail: MyGenericList[Nothing]                       = undef
+  override def drop(n: Int): MyGenericList[Nothing]               = if (n == 0) this else undef
+  override def take(n: Int): MyGenericList[Nothing]               = if (n == 0) this else undef
+  override def map[T](f: Nothing => T): MyGenericList[T]          = MyNil
+  override def filter[T](f: Nothing => Boolean): MyGenericList[T] = MyNil
+}
+
+object Comparator {
+  def ComparatorGList2[T <: Int]: Comparator[MyGenericList[T]]  =
+    (a: MyGenericList[T], b: MyGenericList[T]) => sum(a)(0) compare sum(b)(0)
+  def turnerStr2: (String, String) => String                    = {_ + _}
+}
---
+++ 04/practice/src/test/scala/org/spbsu/mkn/scala/MyGenericListTest.scala
@@ -0,0 +1,97 @@
+package org.spbsu.mkn.scala
+
+import org.scalatest.funsuite.AnyFunSuite
+import org.spbsu.mkn.scala.MyGenericList.{ComparatorGList, MyNil, fromSeq, plus, size, sort, sum, turnerInt, turnerStr}
+
+class MyGenericListTest extends AnyFunSuite {
+  test("head") {
+    assert(fromSeq(Seq(1,2,3)).head == 1)
+    assert(fromSeq(Seq(1)).head == 1)
+    assertThrows[UnsupportedOperationException](fromSeq(Seq()).head)
+  }
+
+  test("tail") {
+    assert(fromSeq(Seq(1,2,3)).tail == fromSeq(Seq(2,3)))
+    assert(fromSeq(Seq(1)).tail == MyNil)
+  }
+
+  test("drop") {
+    assert(fromSeq(Seq(1,2,3)).drop(0) == fromSeq(Seq(1,2,3)))
+    assert(fromSeq(Seq(1,2,3)).drop(2) == fromSeq(Seq(3)))
+    assert(fromSeq(Seq(1,2,3)).drop(3) == MyNil)
+    assertThrows[UnsupportedOperationException](fromSeq(Seq(1,2,3)).drop(10))
+  }
+
+  test("take") {
+    assert(fromSeq(Seq(1,2,3)).take(0) == MyNil)
+    assert(fromSeq(Seq(1,2,3)).take(2) == fromSeq(Seq(1,2)))
+    assert(fromSeq(Seq(1,2,3)).take(3) == fromSeq(Seq(1,2,3)))
+    assertThrows[UnsupportedOperationException](fromSeq(Seq(1,2,3)).take(10))
+  }
+
+  test("map") {
+    assert(MyNil[Int].map(_ * 2) == MyNil)
+    assert(fromSeq(Seq(1,2,3)).map(_ * 2) == fromSeq(Seq(2,4,6)))
+    assert(fromSeq(Seq(1,2,3)).map(identity) == fromSeq(Seq(1,2,3)))
+  }
+
+  test("size") {
+    assert(size(MyNil) == 0)
+    assert(size(fromSeq(Seq(1,2,3))) == 3)
+  }
+
+  test("sum") {
+    assertThrows[UnsupportedOperationException](sum(MyNil[Int])(0))
+    assert(sum(fromSeq(Seq(1,2,3)))(0) == 6)
+    assert(sum(fromSeq(Seq(1)))(0) == 1)
+    assert(sum(fromSeq(Seq("a", "bc", "de", "f")))("t")(Comparator.turnerStr2) == "tabcdef")
+    assert(sum(fromSeq(Seq("a", "bc", "de", "f")))(0) == 6)
+  }
+
+  test("filter") {
+    assert(fromSeq(Seq(1,2,3)).filter(_ == 2) == fromSeq(Seq(2)))
+    assert(fromSeq(Seq(1,2,3)).filter(_ != 2) == fromSeq(Seq(1,3)))
+    assert(fromSeq(Seq(1,2,3)).filter(_ > 6) == MyNil)
+    assert(MyNil[Int].filter(_ == 1) == MyNil)
+    assert(MyNil[Int].filter(_ != 1) == MyNil)
+  }
+
+  test("covariance") {
+    abstract class Animal {
+      def name: String
+    }
+    case class Cat(name: String) extends Animal
+    case class Dog(name: String) extends Animal
+
+    def animalListSize(animal: MyGenericList[Animal]): Int = size(animal)
+
+    val cats: MyGenericList[Cat] = fromSeq(Seq(Cat("Cat1"), Cat("Cat2")))
+    val dogs: MyGenericList[Dog] = fromSeq(Seq(Dog("Dog1"), Dog("Dog2")))
+    assert (animalListSize(cats) == 2)
+    assert (animalListSize(dogs) == 2)
+  }
+
+  test("plus") {
+    assert(plus(fromSeq(Seq(1,2,3)),fromSeq(Seq(4,5,6))) == fromSeq(Seq(1,2,3,4,5,6)))
+    assert(plus(MyNil,fromSeq(Seq(4,5,6))) == fromSeq(Seq(4,5,6)))
+    assert(plus(fromSeq(Seq(1,2,3)),MyNil) == fromSeq(Seq(1,2,3)))
+    assert(plus(MyNil,MyNil) == MyNil)
+  }
+
+  test("sort") {
+    assert(sort(fromSeq(Seq(1,2,3)))(Ordering[Int]) == fromSeq(Seq(3,2,1)))
+    assert(sort(fromSeq(Seq(1,2,3)))(Ordering[Int].reverse) == fromSeq(Seq(1,2,3)))
+    assert(sort(fromSeq(Seq(25,30,75,42,39,25,8,31,98,12,59,23,7,60,18)))(Ordering[Int].reverse)  ==
+      fromSeq(Seq(7,8,12,18,23,25,25,30,31,39,42,59,60,75,98)))
+    assert(sort(fromSeq(Seq(5,2,21,2,3,2,5,1,5,2)))(Ordering[Int].reverse) ==
+      fromSeq(Seq(1,2,2,2,2,3,5,5,5,21)))
+    assert(sort(fromSeq(Seq(fromSeq(Seq(1,2)), MyNil, fromSeq(Seq(1)), MyNil, fromSeq(Seq(3,1,2)))))==
+      fromSeq(Seq(fromSeq(Seq(3,1,2)), fromSeq(Seq(1,2)), fromSeq(Seq(1)), MyNil, MyNil)))
+    assert(sort(fromSeq(Seq(fromSeq(Seq(MyNil, MyNil)), fromSeq(Seq(MyNil, Seq(fromSeq(Seq(1,2)))))))) ==
+      fromSeq(Seq(fromSeq(Seq(MyNil, Seq(fromSeq(Seq(1,2))))), fromSeq(Seq(MyNil, MyNil)))))
+    assert(sort(fromSeq(Seq(fromSeq(Seq(1)), fromSeq(Seq(5)), fromSeq(Seq(2,2))))) ==
+      fromSeq(Seq(fromSeq(Seq(2,2)), fromSeq(Seq(5)), fromSeq(Seq(1)))))
+    assert(sort(fromSeq(Seq(fromSeq(Seq(1)), fromSeq(Seq(5)), fromSeq(Seq(2,2)))))(Comparator.ComparatorGList2) ==
+      fromSeq(Seq(fromSeq(Seq(5)), fromSeq(Seq(2,2)), fromSeq(Seq(1)))))
+  }
+}
